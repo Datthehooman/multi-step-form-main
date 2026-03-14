c6bc788e5882e0b923cfe42f705f68c86e14ba2b
Datthehooman <datbui13579@gmail.com>
2026-03-14 08:19:23 +0700

    setup breakpoint

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+  },
+};
+
+export default config;
